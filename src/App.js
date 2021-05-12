61f896e33959cfb006af27533cb9b3da44e6cff9
Achmad Fauzi <achmadfauzi@jiboncoco.local>
2021-05-13 01:21:52 +0700

    update all

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Header from './components/Header.js';
 import Hero from './components/Hero.js';
 import Browse from './components/Browse.js';
 import Arrived from './components/Arrived.js';
+import Clients from './components/Clients.js';
+import AsideMenu from './components/AsideMenu.js';
+import Footer from './components/Footer.js';
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
       <Hero />
       <Browse />
       <Arrived />
+      <Clients />
+      <AsideMenu />
+      <Footer />
     </>
   );
 }
