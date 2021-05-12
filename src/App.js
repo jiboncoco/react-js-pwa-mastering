0c08989c2a07d3684e1a06cc7c9e57b05fdaffd8
Achmad Fauzi <achmadfauzi@jiboncoco.local>
2021-05-12 16:08:55 +0700

    update header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
+import Header from './components/Header.js';
 
 function App() {
   return (
-    <div className="max-w-md mx-auto flex p-6 bg-gray-100 mt-10 rounded-lg ">
-      <h1 class="text-2xl text-blue-700 leading-tight">Hello World</h1>
-    </div>
+    <Header />
   );
 }
 
