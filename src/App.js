698d43fcb00b05d2fdddcd715e5f6e4d99477028
Achmad Fauzi <achmadfauzi@jiboncoco.local>
2021-05-13 00:47:52 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from './components/Header.js';
 import Hero from './components/Hero.js';
+import Browse from './components/Browse.js';
+import Arrived from './components/Arrived.js';
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <Browse />
+      <Arrived />
     </>
   );
 }
